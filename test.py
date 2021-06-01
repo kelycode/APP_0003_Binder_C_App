114b6f05c44fdb95eee732a3d583bb11f32dcd27
zhuk <zk01066273@antgroup.com>
2021-06-01 11:04:21 +0800

    #Fix run error

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import time
 
 count=0
 while(True):
-    count++
-    print("Hello count = "+count)
-    sleep(1)
+    count = count+1
+    print("Hello count = "+str(count))
+    time.sleep(1)
+    if(count > 60):
+        break
