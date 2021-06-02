00847643b66f13ceb28ace41b8d163cc17085f66
zhuk <zk01066273@antgroup.com>
2021-06-02 11:37:34 +0800

    #Feature change print.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import time
 count=0
 while(True):
     count = count+1
-    print("Hello count = "+str(count))
+    print("Hello test count = "+str(count))
     time.sleep(1)
     if(count > 60):
         break
